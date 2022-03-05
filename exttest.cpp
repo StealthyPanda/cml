025361576e7f76fc4026ab16e8225ddcc2f8d32a
StealthyPanda <shaikm259@gmail.com>
2022-03-05 16:13:43 +0400

    Stash

--- exttest.cpp
+++ exttest.cpp
@@ -3,10 +3,28 @@
 #include <quadmath.h>
 #include "ml.h"
 
-int main()
+/*int main()
 {
 
 	NeuralNetwork bruh = NeuralNetwork::extract("bruh16.nn");
+
+	bruh.print();
+
+	Trainer tr(bruh, "test2.td", 1000);
+	tr.partition();
+
+	std::cout << tr.calculatecost(tr.trainingdatasets, tr.ntraining) << std::endl;
+	std::cout << tr.calculatecost(tr.testingdatasets, tr.ntesting) << std::endl;
+
+	return 0;
+}*/
+
+int main(int argc, char const *argv[])
+{
+	//std::string name = "networks/ellipseboi" + std::to_string(argv[0]) + ".nn";
+
+	std::cout << argv[1] << std::endl;
+	NeuralNetwork bruh = NeuralNetwork::extract(argv[1]);
 
 	bruh.print();
 
